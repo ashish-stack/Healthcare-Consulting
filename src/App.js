dc4f0d1edc92f7e1d95ce9085e45e7248a65679b
Ashish <vishwakarmaashish832@gmail.com>
2022-08-20 17:31:05 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <h1>App</h1>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/coachlogin" element={<CoachLogin />}></Route>
+            <Route path="/coachsignup" element={<CoachSignup />}></Route>
+            <Route path="/userlogin" element={<UserLogin />}></Route>
+            <Route path="/usersignup" element={<UserSignup />}></Route>
+            <Route path="/coachhome" element={<CoachHome />}></Route>
+            <Route path="/coachschedules" element={<CoachHome />}></Route>
+            <Route path="/userhome" element={<UserHome />}></Route>
+            <Route path="/coachviewprofile" element={<CoachViewProfile />}></Route>
+            <Route path="/userviewprofile" element={<UserViewProfile />}></Route>
+            <Route path="/userappointments" element={<UserAppointments />}></Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
