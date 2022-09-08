3199fd558109e485117e9bde2d54dca4e2978499
Ashish <vishwakarmaashish832@gmail.com>
2022-09-08 20:34:36 +0530

    Reschedule and Delete added

--- src/Components/RescheduleAndDelete.js
+++ src/Components/RescheduleAndDelete.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import '../Css/UserHome.css';
 
 function RescheduleAndDelete(){
     const [second, setSecond] = useState(false);
